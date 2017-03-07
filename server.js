f6e48c6f85f989cc18cd1285a9e412ca6c11cb95
amyhagelin <ahagelin@gmail.com>
2017-03-06 22:01:43 -0800

    minor updates to test flow

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const DATABASE_URL = process.env.DATABASE_URL ||
-                       global.DATABASE_URL ||
-                      'mongodb://localhost/blog-app';
+                     global.DATABASE_URL ||
+                     'mongodb://localhost/blog-app';
 const PORT = process.env.PORT || 8080;
 
 var express = require('express');
