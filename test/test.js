ae87e7e79ea13c10dccf02522ddb3f3ced7b2a51
amyhagelin <ahagelin@gmail.com>
2017-03-28 22:06:15 -0700

    cleaning up code, updates to test

--- test/test.js
+++ test/test.js
@@ -73,6 +73,32 @@ describe('Orders API resource', function() {
 				});
 		})
 
+// PUT TEST
+
+	const updateData = {name: 'Updated Name', email: 'updated@email.com', phone: '123456789'};
+
+		it('should update an existing order', function() {
+			return Orders
+			.findOne()
+			.exec()
+			.then(function(order){
+				updateData._id = order._id;
+				return chai.request(app)
+					.put(`/orders/${order._id}`)
+					.send(updateData);
+			})
+			.then(function(res){
+				res.should.have.status(204);
+
+				return Orders.findById(updateData._id).exec();
+			})
+			.then(function(order) {
+				order.name.should.equal(updateData.name);
+				order.email.should.equal(updateData.email);
+				order.phone.should.equal(updateData.phone);
+			});
+		})
+
 
 	// DELETE TEST
 	it('should delete an order', function() {
@@ -95,32 +121,7 @@ describe('Orders API resource', function() {
 			})	
 	})
 
-	// PUT TEST
-
-	// const updateData = {name: 'Updated Name', email: 'updated@email.com', phone: '123456789'};
-
-	// 	it('should update an existing order', function() {
-	// 		return Orders
-	// 		.findOne()
-	// 		.exec()
-	// 		.then(function(order){
-	// 			updateData._id = order._id;
-	// 			return chai.request(app)
-	// 				.put(`/orders/${order._id}`)
-	// 				.send(updateData);
-	// 		})
-	// 		.then(function(res){
-	// 			res.should.have.status(204);
-
-	// 			return Orders.findById(updateData._id).exec();
-	// 		})
-	// 		.then(function(order) {
-	// 			order.name.should.equal(updateData.name);
-	// 			order.email.should.equal(updateData.email);
-	// 			order.phone.should.equal(updateData.phone);
-	// 		});
-	// 	})
-
+	
 
 
 
